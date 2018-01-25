d037d3c14ec5f68598655036221848c13367262c
sabfir <oleg.sabfir@gmail.com>
2018-01-25 18:55:12 +0200

    add gateway and adapter example

---
+++ spring-integration-jms-dsl-between-two-app_2_replier/src/main/java/com/opinta/AppGatewayExample.java
@@ -0,0 +1,48 @@
+//package com.opinta;
+//
+//import javax.jms.ConnectionFactory;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.boot.ApplicationArguments;
+//import org.springframework.boot.ApplicationRunner;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.ImportResource;
+//import org.springframework.integration.dsl.IntegrationFlow;
+//import org.springframework.integration.dsl.IntegrationFlows;
+//import org.springframework.integration.dsl.jms.Jms;
+//
+//@SpringBootApplication
+//@Configuration
+//@ImportResource("classpath:integration-context.xml")
+//public class AppGatewayExample implements ApplicationRunner {
+//    @Value("${queue.boot}")
+//    private String destination;
+//    @Value("${queue.boot-reply}")
+//    private String destinationReply;
+//
+//    public static void main( String[] args ) {
+//        SpringApplication.run(AppGatewayExample.class, args);
+//    }
+//
+//    @Override
+//    public void run(ApplicationArguments applicationArguments) {
+//        System.out.println("START - spring-integration-jms-between-two-app_2 - GATEWAY");
+//    }
+//
+//    @Bean
+//    public IntegrationFlow inboundFlow(ConnectionFactory connectionFactory) {
+//        return IntegrationFlows
+//                .from(Jms.inboundGateway(connectionFactory)
+//                        .destination(destination)
+////                        .defaultReplyDestination(new ActiveMQTempQueue(destinationReply)) // it will use it only if the message doesn't content data in JMSReplyTo field
+//                )
+//                .transform((String payload) -> {
+//                    System.out.println("GATEWAY_MESSAGE_READ: " + payload);
+//                    return payload + " -> RECEIVER_APP -> SEND_REPLY";
+//                })
+//                .get();
+//    }
+//}
